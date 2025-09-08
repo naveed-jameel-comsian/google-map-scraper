db1681a2ee118c216ea6a92dc425388416e294c5
haroonrashid786 <haroonrashid786uk@gmail.com>
2025-09-08 13:34:55 +0500

    integrating ci/cd pipeline

---
+++ .scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo "🚀 Deployment started ..."
+
+# Move to the project folder directory
+echo "📂 Moving to project folder directory"
+cd /home/ubuntu/scrapper-project
+
+# Pull the latest version of the app
+echo "📥 Pulling latest changes..."
+git pull origin main
+echo "✅ New changes copied to server!"
+
+# Activate Virtual Env
+source venv/bin/activate
+echo "Virtual env 'venv' activated!"
+
+# Install dependencies
+echo "📦 Installing dependencies..."
+pip install -r requirements.txt
+
+# Deactivate Virtual Env
+deactivate
+echo "Virtual env 'venv' deactivated!"
+
+# Reload services
+echo "Restarting Nginx & Gunicorn..."
+sudo systemctl daemon-reload
+sudo systemctl restart nginx
+sudo systemctl restart fastapi
+echo "✅ Services restarted!"
+
+echo "Deployment Finished!"
